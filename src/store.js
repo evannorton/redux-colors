948151ba602158948d391220731e26fce5eb3442
Anna Hope Sherman <anna.sherman365@gmail.com>
2019-08-21 11:03:38 -0400

    Adds redux, react-redux and ramda packages Adds redux setup with store

---
+++ src/store.js
@@ -0,0 +1,14 @@
+import { createStore, applyMiddleware } from "redux";
+import { compose } from "redux";
+import rootReducer from "./reducers";
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export default function configureStore() {
+  const store = createStore(
+    rootReducer,
+    composeEnhancers()
+  );
+
+  return store;
+}

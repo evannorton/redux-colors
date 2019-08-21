cc72cb7320e4861ed5abe603c309255e43aebe7e
Anna Hope Sherman <anna.sherman365@gmail.com>
2019-08-21 12:50:52 -0400

    Adds dummy close modal button

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
 function Modal() {
-  return <div className="Modal">I am a modal!</div>;
+  return <div className="Modal">
+    <div>I am a modal!</div>
+    <button type="button" onClick={() => {}}>X</button>
+  </div>;
 }
 
 export default Modal;

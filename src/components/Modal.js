17aff55445699429266f774f95f18772a21ebc71
Anna Hope Sherman <anna.sherman365@gmail.com>
2019-08-21 12:59:40 -0400

    Hooks up modal close button to hide modal action

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { hideModal } from '../actions/modalAction';
 
-function Modal() {
+function Modal(props) {
+  const { handleHideModal } = props;
   return <div className="Modal">
     <div>I am a modal!</div>
-    <button type="button" onClick={() => {}}>X</button>
+    <button type="button" onClick={handleHideModal}>X</button>
   </div>;
 }
 
-export default Modal;
+Modal.propTypes = {
+  handleHideModal: PropTypes.func.isRequired,
+};
+
+const mapDispatchToProps = dispatch => bindActionCreators(
+  { handleHideModal: hideModal }, dispatch);
+
+export default connect(null, mapDispatchToProps)(Modal);
--- src/reducers/modalReducer.js
+++ src/reducers/modalReducer.js
@@ -1,5 +1,5 @@
 import { mergeRight } from 'ramda';
-import { SHOW_MODAL } from '../actions/modalAction';
+import { SHOW_MODAL, HIDE_MODAL } from '../actions/modalAction';
 
 const initialState = { display: false };
 
@@ -7,6 +7,8 @@ export default function modal(state = initialState, action) {
   switch (action.type) {
     case SHOW_MODAL:
       return mergeRight(state, { display: true});
+    case HIDE_MODAL:
+      return mergeRight(state, { display: false });
     default:
       return state;
   }

a69059ce8ec41a41904fa448c95c4158708342c4
evan <evanan39@yahoo.com>
2019-08-21 21:58:02 -0400

    Updates

---
+++ precache-manifest.8787b02a40965bdb0f620504a17f4737.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86ec6b656110a9ceed047bdb38fa4c26",
+    "url": "/redux-colors/index.html"
+  },
+  {
+    "revision": "117d399dd01cad4fb262",
+    "url": "/redux-colors/static/css/main.e9639a40.chunk.css"
+  },
+  {
+    "revision": "917f2157ddcf42cd5130",
+    "url": "/redux-colors/static/js/2.a0936545.chunk.js"
+  },
+  {
+    "revision": "117d399dd01cad4fb262",
+    "url": "/redux-colors/static/js/main.a900224e.chunk.js"
+  },
+  {
+    "revision": "f89b948cc83bfe18fc0f",
+    "url": "/redux-colors/static/js/runtime~main.97d39e16.js"
+  }
+]);
